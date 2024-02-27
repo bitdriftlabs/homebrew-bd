ef826be7a4766882ea99892df7efa713a19535fe
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-02-27 11:29:25 -0500

    s

--- Formula/bd.rb
+++ Formula/bd.rb
@@ -4,7 +4,7 @@
 class Bd < Formula
   desc "bitdrift CLI tool"
   homepage "https://bitdrift.io"
-  url "https://dl.bitdrift.io/bd-cli/0.1.8/bd-cli-mac-arm64.tar.gz"
+  url "https://dl.bitdrift.io/bd-cli/0.1.9/bd-cli-mac-arm64.tar.gz"
   sha256 "064ead4fc19bcac5d6142c47760dbde50be84639c7e215af264bd377cc407331"
   version "0.1.9"
 
