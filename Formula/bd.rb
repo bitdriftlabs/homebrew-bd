79028970b4309bf503e4c9d7c3e44b679f680f8a
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-03-12 16:50:02 -0400

    release 0.1.16

--- Formula/bd.rb
+++ Formula/bd.rb
@@ -4,9 +4,9 @@
 class Bd < Formula
     desc "bitdrift CLI tool"
     homepage "https://bitdrift.io"
-    url "https://dl.bitdrift.io/bd-cli/0.1.15/bd-cli-mac-arm64.tar.gz"
-    sha256 "f54d022ac2bd672a8b3dd04a628d988f5784c44f8a003ee327c5ae787a49ea87"
-    version "0.1.15"
+    url "https://dl.bitdrift.io/bd-cli/0.1.16/bd-cli-mac-arm64.tar.gz"
+    sha256 "c8489e4320769155f76775a4ecb3998c3f0300edb9f90b39fafe42d8b4066aa6"
+    version "0.1.16"
   
     def install
       bin.install "bd"
