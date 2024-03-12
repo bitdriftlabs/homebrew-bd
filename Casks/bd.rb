ace56dfbe11fce242f7651234d0debcd8f72021b
Rafal Augustyniak <rafalaugustyniak@gmail.com>
2024-03-12 15:17:07 -0400

    release 0.1.5

--- Casks/bd.rb
+++ Casks/bd.rb
@@ -2,8 +2,8 @@
 #                https://rubydoc.brew.sh/Formula
 cask "bd" do
   arch arm: "-arm64"
-  version "0.1.14"
-  sha256 arm: "37a92b7de853882e4a6ca429f921098efa19647b55ab754c410636ad2fe8eccd"
+  version "0.1.15"
+  sha256 arm: "f54d022ac2bd672a8b3dd04a628d988f5784c44f8a003ee327c5ae787a49ea87"
 
   desc "bitdrift CLI tool"
   homepage "https://bitdrift.io"
