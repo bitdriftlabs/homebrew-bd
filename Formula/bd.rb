2dc3d3056bb648d81b34ac42c6142508ac7e04ca
Matt Klein <mattklein123@gmail.com>
2025-11-03 13:21:23 -0700

    Fix release (#27)
    Signed-off-by: Matt Klein <mattklein123@gmail.com>

--- Formula/bd.rb
+++ Formula/bd.rb
@@ -5,7 +5,7 @@ class Bd < Formula
     desc "bitdrift CLI tool"
     homepage "https://bitdrift.io"
     url "https://dl.bitdrift.io/bd-cli/0.1.35/bd-cli-mac-universal-apple-darwin.tar.gz"
-    sha256 "159084238e58d5cd8a90688d6616e792fb0b879aad44ae2a8eed78609072a660"
+    sha256 "7f147b6d1072abb1c8a08d3f208551d36d41851553a37a58d3c344a27e254df3"
     version "0.1.35"
   
     def install
